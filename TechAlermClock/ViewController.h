8df393bb9f4471d3235eb4cc0ac50ed8d3c7367b
Rey <quit9909@icloud.com>
2015-03-16 23:16:09 +0900

    Initial Commit

---
+++ TechAlermClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechAlermClock
+//
+//  Created by ReyMasuda on 2015/03/16.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TechAlermClock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TechAlermClock
+//
+//  Created by ReyMasuda on 2015/03/16.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
